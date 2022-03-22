ac041b71626b7946aea299b2ae04c0280f50b719
JCMP92 <arq_juanmp@outlook.com>
2022-03-22 08:30:09 -0600

    config changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { navBar } from "./navigation bar/navBar";
 import { pageInit } from "./home/homePage";
-import { menuInit } from "./menu/menuPage";
-import { contactInit } from "./contact/contactPage";
+// import { menuInit } from "./menu/menuPage";
+// import { contactInit } from "./contact/contactPage";
+import"normalize.css";
+import"./style/style.scss";
--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,23 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules:[
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
 };
