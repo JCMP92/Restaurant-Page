6d5df473cca7a99a384aabae627625623bcb46fc
JCMP92 <arq_juanmp@outlook.com>
2022-03-22 17:46:55 -0600

    styling the menu, in process

--- src/menu/menuPage.js
+++ src/menu/menuPage.js
@@ -5,14 +5,26 @@ import { pageHeader } from "../general-dependencies/pageTitles";
 
 const menuInit = (() =>{
     const container =  document.getElementById('content');
+    const menuContainer = document.createElement('div');
+        menuContainer.classList.add('mnu-container');
+     
+        const titleContent = document.createElement('div');
+            titleContent.classList.add('title-content')
+        menuContainer.appendChild(titleContent);
+
+        const menuContent = document.createElement('div');
+            menuContent.classList.add('mnu-content');
+        menuContainer.appendChild(menuContent); 
+    
+    container.appendChild(menuContainer);
 
     //Page title
     const menuTitle = pageHeader('Menú')
-    container.appendChild(menuTitle);
+    titleContent.appendChild(menuTitle);
     
     // First Section
     const firstSection = menuSection('Bebidas calientes');
-    container.appendChild(firstSection);
+    menuContent.appendChild(firstSection);
 
     //Menu elements for their respective sections, we are appending them to the variable that we declared as firstSection.
     const cafeAmericano = menuItem('Café Americano', 'Lorem ipsum dolor', '35.00');
@@ -24,7 +36,7 @@ const menuInit = (() =>{
     
     // Second Section
     const secondSection = menuSection('Métodos de extracción');
-    container.appendChild(secondSection);
+    menuContent.appendChild(secondSection);
 
     //Menu elements for their respective sections, we are appending them to the variable that we declared as secondSection.
     const chemex = menuItem('Chemex', 'Lorem ipsum dolor', '60.00');
