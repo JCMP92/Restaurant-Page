adce76244d14fc5a814ceb9e942ae7d80569dfab
JCMP92 <arq_juanmp@outlook.com>
2022-03-23 10:24:09 -0600

    Added some menu items

--- src/menu/menuPage.js
+++ src/menu/menuPage.js
@@ -15,15 +15,23 @@ const menuInit = (() =>{
         const menuContent = document.createElement('div');
             menuContent.classList.add('mnu-content');
         menuContainer.appendChild(menuContent); 
+
+        const titleContentTwo = document.createElement('div');
+            titleContentTwo.classList.add('title-content-two')
+        menuContainer.appendChild(titleContentTwo);
+
+        const menuContentTwo = document.createElement('div');
+            menuContentTwo.classList.add('mnu-content');
+        menuContainer.appendChild(menuContentTwo); 
     
     container.appendChild(menuContainer);
 
-    //Page title
-    const menuTitle = pageHeader('Menú')
-    titleContent.appendChild(menuTitle);
+    //Menu title
+    const menuTitleDrink = pageHeader('Bebidas')
+    titleContent.appendChild(menuTitleDrink);
     
     // First Section
-    const firstSection = menuSection('Bebidas calientes');
+    const firstSection = menuSection('Calientes');
     menuContent.appendChild(firstSection);
 
     //Menu elements for their respective sections, we are appending them to the variable that we declared as firstSection.
@@ -35,17 +43,60 @@ const menuInit = (() =>{
     firstSection.appendChild(moka);  
     
     // Second Section
-    const secondSection = menuSection('Métodos de extracción');
+    const secondSection = menuSection('Frías');
     menuContent.appendChild(secondSection);
 
     //Menu elements for their respective sections, we are appending them to the variable that we declared as secondSection.
-    const chemex = menuItem('Chemex', 'Lorem ipsum dolor', '60.00');
-    secondSection.appendChild(chemex); 
-    const v60 = menuItem('V60', 'Lorem ipsum dolor', '60.00');
-    secondSection.appendChild(v60); 
-    const aeropress = menuItem('Aeropress', 'Lorem ipsum dolor', '60.00');
-    secondSection.appendChild(aeropress); 
+    const tisana = menuItem('Tisana', 'Lorem ipsum dolor', '40.00');
+    secondSection.appendChild(tisana); 
+    const ocaso = menuItem('Ocaso', 'Lorem ipsum dolor', '35.00');
+    secondSection.appendChild(ocaso); 
+    const coldBrew = menuItem('Cold Brew', 'Lorem ipsum dolor', '60.00');
+    secondSection.appendChild(coldBrew); 
 
+    // Third Section
+    const thirdSection = menuSection('Métodos de extracción');
+    menuContent.appendChild(thirdSection);
+
+    //Menu elements for their respective sections, we are appending them to the variable that we declared as secondSection.
+    const chemex = menuItem('Chemex', 'Lorem ipsum dolor', '60.00');
+    thirdSection.appendChild(chemex); 
+    const v60 = menuItem('V60', 'Lorem ipsum dolor', '60.00');
+    thirdSection.appendChild(v60); 
+    const aeropress = menuItem('Aeropress', 'Lorem ipsum dolor', '60.00');
+    thirdSection.appendChild(aeropress); 
+
+
+
+
+    //Menu title
+    const menuTitleFood = pageHeader('Comida');
+        menuTitleFood.classList.add('mnu-food-title');
+    titleContentTwo.appendChild(menuTitleFood);
+
+    // Fourth Section
+    const frthSection = menuSection('Sándwich');
+    menuContentTwo.appendChild(frthSection);
+
+    //Menu elements for their respective sections, we are appending them to the variable that we declared as frthSection.
+    const special = menuItem('KFFTO Especial', 'Lorem ipsum dolor', '60.00');
+    frthSection.appendChild(special); 
+    const tuna = menuItem('Atún', 'Lorem ipsum dolor', '45.00');
+    frthSection.appendChild(tuna); 
+    const veggie = menuItem('Cold Brew', 'Lorem ipsum dolor', '45.00');
+    frthSection.appendChild(veggie);   
+    
+    // Fifth Section
+    const fifthSection = menuSection('Postres');
+    menuContentTwo.appendChild(fifthSection);
+
+    //Menu elements for their respective sections, we are appending them to the variable that we declared as frthSection.
+    const cookie = menuItem('Galleta KFFTO', 'Lorem ipsum dolor', '30.00');
+    fifthSection.appendChild(cookie); 
+    const brownie = menuItem('Brownie', 'Lorem ipsum dolor', '45.00');
+    fifthSection.appendChild(brownie); 
+    const pie = menuItem('Tartas', 'Lorem ipsum dolor', '45.00');
+    fifthSection.appendChild(pie);  
 })
 ();
 
