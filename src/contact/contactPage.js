383edec97d15707622813460ded764b56e0d1f1a
JCMP92 <arq_juanmp@outlook.com>
2022-03-23 17:29:30 -0600

    contact page styling done

--- src/contact/contactPage.js
+++ src/contact/contactPage.js
@@ -5,18 +5,33 @@ import { socialMedia } from "./contact-dependencies/socialMediaCreator";
 
 const contactInit = (() => {
     const container =  document.getElementById('content');
+    const contactContainer = document.createElement('div');
+        contactContainer.classList.add('contact-container');
+    container.appendChild(contactContainer);  
 
     //Page title
-    const menuTitle = pageHeader('Contacto')
-    container.appendChild(menuTitle);
+    const menuTitle = pageHeader('Vive la experiecia del café');
+    const titleContainer = document.createElement('div');
+        titleContainer.classList.add('title-div');
+
+    titleContainer.appendChild(menuTitle);
+    contactContainer.appendChild(titleContainer);
 
     //Contact card
-    const myContact = contactCard('Bienvenido', 2222002200);
-    container.appendChild(myContact);
+    const myContact = contactCard('¡Bienvenido!', 2222002200);
+    const phoneContainer = document.createElement('div');
+        phoneContainer.classList.add('phone-div');
+
+    phoneContainer.appendChild(myContact);
+    contactContainer.appendChild(phoneContainer);
 
     //Social media div
     const myMedia = socialMedia();
-    container.appendChild(myMedia);
+    const mediaContainer = document.createElement('div');
+        mediaContainer.classList.add('media-div');
+
+    mediaContainer.appendChild( myMedia);
+    contactContainer.appendChild(mediaContainer);
 
 })
 ();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { navBar } from "./navigation bar/navBar";
 // import { pageInit } from "./home/homePage";
 // import { identityInit } from "./whoWeAre/whoWeAre";
-import { menuInit } from "./menu/menuPage";
-// import { contactInit } from "./contact/contactPage";
+// import { menuInit } from "./menu/menuPage";
+import { contactInit } from "./contact/contactPage";
 import"normalize.css";
 import"./style/style.scss";
