f0c482e66d8648d6f76ed97b19febda92cdb3d71
JCMP92 <arq_juanmp@outlook.com>
2022-04-19 12:14:47 -0500

    Added ESLint

--- src/index.js
+++ src/index.js
@@ -4,20 +4,20 @@ import { identityInit } from "./whoWeAre/whoWeAre";
 import { menuInit } from "./menu/menuPage";
 import { contactInit } from "./contact/contactPage";
 
-import { homeNavigation, menuNavigation, identityNavigation, contactNavigation, } from "./tabNavFunction/tabNav";
+import {
+  homeNavigation, menuNavigation, identityNavigation, contactNavigation,
+} from "./tabNavFunction/tabNav";
 
-
-import"normalize.css";
-import"./style/style.scss";
+import "normalize.css";
+import "./style/style.scss";
 
 const landingHome = pageInit();
 
-const homeButton = document.getElementById('home-button');
-    homeButton.addEventListener('click', homeNavigation);
-const aboutButton = document.getElementById('about-button');
-    aboutButton.addEventListener('click', identityNavigation);
-const menuButton = document.getElementById('menu-button');
-    menuButton.addEventListener('click', menuNavigation);   
-const contactButton = document.getElementById('contact-button');
-    contactButton.addEventListener('click', contactNavigation);   
-
+const homeButton = document.getElementById("home-button");
+homeButton.addEventListener("click", homeNavigation);
+const aboutButton = document.getElementById("about-button");
+aboutButton.addEventListener("click", identityNavigation);
+const menuButton = document.getElementById("menu-button");
+menuButton.addEventListener("click", menuNavigation);
+const contactButton = document.getElementById("contact-button");
+contactButton.addEventListener("click", contactNavigation);
